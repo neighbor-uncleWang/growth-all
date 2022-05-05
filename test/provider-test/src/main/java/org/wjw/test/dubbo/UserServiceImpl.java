f8eadfc095b03fc1e34de6cdf1b910601ab3c94e
隔壁老王 <1103450854@qq.com>
2022-05-05 22:54:49 +0800

    dubbo demo 依赖添加

---
+++ test/provider-test/src/main/java/org/wjw/test/dubbo/UserServiceImpl.java
@@ -0,0 +1,20 @@
+package org.wjw.test.dubbo;
+
+import org.wjw.test.dubbo.service.UserService;
+
+/**
+ * @author wjw
+ * @ClassName UserServiceImpl.java
+ * @createTime 2022-05-05 22:11
+ */
+public class UserServiceImpl implements UserService {
+    @Override
+    public String queryUser(String userId) {
+        return null;
+    }
+
+    @Override
+    public void doKill(String killid) {
+
+    }
+}
