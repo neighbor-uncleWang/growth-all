24b659fd3d831e5e30add9c17aeec970116b71d8
隔壁老王 <1103450854@qq.com>
2022-05-18 21:36:09 +0800

    cloud项目多模块初始化

---
+++ cloud-all/biz-service/goods-service/src/test/java/com/wjw/good/GoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjw.good;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ cloud-all/biz-service/marking-service/src/main/java/com/wjw/marking/MarkingApplication.java
@@ -0,0 +1,13 @@
+package com.wjw.marking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarkingApplication.class, args);
+    }
+
+}
