2acc43b03a3bfc31f61f20549f79ad33cf9a09dd
隔壁老王 <1103450854@qq.com>
2022-05-19 21:47:29 +0800

    Random初体验

--- cloud-all/portal/src/main/java/com/wjw/portal/web/OrderController.java
+++ cloud-all/portal/src/main/java/com/wjw/portal/web/OrderController.java
@@ -2,6 +2,8 @@ package com.wjw.portal.web;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -13,8 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+import java.util.Random;
+
 /**
- * 订单web
+ * 订单网关web
  *
  * @author laoWang
  * @ClassName OrderController.java
@@ -31,33 +36,33 @@ public class OrderController {
     @Autowired
     private LoadBalancerClient loadbalancerClient;
 
-  /*  @Value("${goodsService.serverList}.split(',')")
-    private List<String> goodsList;*/
+//    @Value("${goodsService.serverList}.split(',')")
+//    private List<String> goodsList;
 
-   /* private String getGoodsServer(){
-        String serverList="http://localhost:9090/goods,http://localhost:9093/goods";
-        String servers[]=serverList.split(",");
-        Random random=new Random();
-        return servers[random.nextInt(servers.length)];
-    }*/
+//    private String getGoodsServer(){
+//        String serverList="http://localhost:9090/goods,http://localhost:9093/goods";
+//        String servers[]=serverList.split(",");
+//        Random random=new Random();
+//        return servers[random.nextInt(servers.length)];
+//    }
 
     @GetMapping
     public String order() {
         log.info("begin do order");
+        ServiceInstance si=loadbalancerClient.choose("goods-service");
+        String url=String.format("http://%s:%s/goods",si.getHost(),si.getPort());
+        log.info("ribbon-url:{}",url);
 
-       /* ServiceInstance si=loadbalancerClient.choose("goods-service");
-        String url=String.format("http://%s:%s",si.getHost(),si.getPort());
-        log.info("ribbon-url:{}",url);*/
-
-        String url = "http://goods-service/goods";
+//        String url = getGoodsServer(); //"http://goods-service/goods";
         String goodsInfo = restTemplate.getForObject(url, String.class);
-        String promotionInfo = restTemplate.getForObject("http://marking-service/promotion", String.class);
-        MultiValueMap<String, Object> param = new LinkedMultiValueMap<>();
-        param.add("goodsInfo", goodsInfo);
-        param.add("pomotionInfo", promotionInfo);
-
-        HttpEntity<MultiValueMap<String, Object>> httpEntity = new HttpEntity<>(param, new HttpHeaders());
-        ResponseEntity<String> response = restTemplate.postForEntity("http://order-service/order", httpEntity, String.class);
-        return response.getBody();
+//        String promotionInfo = restTemplate.getForObject("http://marking-service/promotion", String.class);
+//        MultiValueMap<String, Object> param = new LinkedMultiValueMap<>();
+//        param.add("goodsInfo", goodsInfo);
+//        param.add("pomotionInfo", promotionInfo);
+//
+//        HttpEntity<MultiValueMap<String, Object>> httpEntity = new HttpEntity<>(param, new HttpHeaders());
+//        ResponseEntity<String> response = restTemplate.postForEntity("http://order-service/order", httpEntity, String.class);
+//        return response.getBody();
+        return "1";
     }
 }
