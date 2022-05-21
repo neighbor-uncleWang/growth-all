0ee3c5dc3ba6f4f50a01e25992724c4961c432da
隔壁老王 <1103450854@qq.com>
2022-05-21 11:31:51 +0800

    自定义负载均衡

---
+++ cloud-all/portal/src/main/java/com/wjw/portal/rule/DefinieIpHashRule.java
@@ -0,0 +1,56 @@
+package com.wjw.portal.rule;
+
+import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.AbstractLoadBalancerRule;
+import com.netflix.loadbalancer.ILoadBalancer;
+import com.netflix.loadbalancer.Server;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.List;
+
+/**
+ * @author laoWang
+ * @ClassName DefinieIpHashRule.java
+ * @createTime 2022-05-21 10:57
+ */
+public class DefinieIpHashRule extends AbstractLoadBalancerRule {
+
+    @Override
+    public void initWithNiwsConfig(IClientConfig clientConfig) {
+
+    }
+
+    @Override
+    public Server choose(Object key) {
+        return null;
+    }
+
+
+    public Server choose(ILoadBalancer lb, Object key){
+        if(lb==null){
+            return null;
+        }
+        Server server=null;
+        while(server==null){
+            //表示启动的服务列表.(默认情况下单纯只用Ribbon时，不会对目标服务做心跳检测）
+            List<Server> upList=lb.getReachableServers();
+            List<Server> allList=lb.getAllServers();
+            int serverCount=upList.size();
+            if(serverCount==0){
+                return null;
+            }
+            int index=ipAddressHash(serverCount);
+            server=upList.get(index);
+        }
+        return server;
+    }
+
+    private int ipAddressHash(int serverCount){
+        ServletRequestAttributes requestAttributes= (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        String remoteAddr=requestAttributes.getRequest().getRemoteAddr();
+        int code=Math.abs(remoteAddr.hashCode());
+        return code%serverCount;
+    }
+
+}
