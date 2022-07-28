8bf676498a6f9d7d705d55e4f4e995a140c557f0
王佳伟 <wjw01019072@antgroup.com>
2022-07-28 14:49:16 +0800

    user轻量SDK初始计划

---
+++ cloud-all-2/service-api/src/main/java/com/wjw/cloud/service/IUserService.java
@@ -0,0 +1,17 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2022 All Rights Reserved.
+ */
+package com.wjw.cloud.service;
+
+/**
+ * @author wjw01019072
+ * @version : IUserService.java, v 0.1 2022年07月28日 14:44 wjw01019072 Exp $
+ */
+public interface IUserService {
+    /**
+     * @param msg
+     * @return
+     */
+    String say(String msg);
+}
