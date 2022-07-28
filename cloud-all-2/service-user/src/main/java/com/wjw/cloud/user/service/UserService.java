360982ed6bd5ce576d50641853762f14eaf7674b
王佳伟 <wjw01019072@antgroup.com>
2022-07-28 16:25:17 +0800

    服务初始化

--- cloud-all-2/service-user/src/main/java/com/wjw/cloud/user/service/UserService.java
+++ cloud-all-2/service-user/src/main/java/com/wjw/cloud/user/service/UserService.java
@@ -5,11 +5,13 @@
 package com.wjw.cloud.user.service;
 
 import com.wjw.cloud.service.IUserService;
+import org.apache.dubbo.config.annotation.DubboService;
 
 /**
  * @author wjw01019072
  * @version : UserService.java, v 0.1 2022年07月28日 14:53 wjw01019072 Exp $
  */
+@DubboService(loadbalance = "random")
 public class UserService implements IUserService {
     @Override
     public String say(String msg) {
