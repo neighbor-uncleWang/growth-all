1adcbdab0f1a6b82e1163416547659dcf6b6c77a
王佳伟 <wjw01019072@antgroup.com>
2022-07-28 15:02:47 +0800

    优化

--- cloud-all-2/Gportal/src/main/java/com/wjw/cloud/gportal/GPortalApplication.java
+++ cloud-all-2/Gportal/src/main/java/com/wjw/cloud/gportal/GPortalApplication.java
@@ -6,11 +6,13 @@ package com.wjw.cloud.gportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author wjw01019072
  * @version : GPortalApplication.java, v 0.1 2022年07月28日 14:34 wjw01019072 Exp $
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GPortalApplication {
 
