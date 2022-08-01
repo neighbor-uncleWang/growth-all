b996a920178b15d5b0feca89a271cb0d8fd4dde1
王佳伟 <wjw01019072@antgroup.com>
2022-08-01 11:17:49 +0800

    api服务引入

---
+++ dubbo-all/api/src/main/java/com/wjw/api/IUserService.java
@@ -0,0 +1,18 @@
+package com.wjw.api;
+
+/**
+ * @author laoWang
+ * @ClassName iUserService.java
+ * @createTime 2022-07-31 10:21
+ */
+public interface IUserService {
+
+    /**
+     * 说
+     *
+     * @param msg 消息
+     * @return 返回值
+     */
+    String say(String msg);
+
+}
