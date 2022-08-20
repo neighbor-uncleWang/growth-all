cb6b190683ee61c55424d55fbc1f2f396eea8578
隔壁老王 <1103450854@qq.com>
2022-08-20 19:31:53 +0800

    shop创建

---
+++ cloud-all-2/service-shop/src/main/java/com/wjw/cloud/shop/ShopServiceApplication.java
@@ -0,0 +1,23 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2022 All Rights Reserved.
+ */
+package com.wjw.cloud.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author wjw01019072
+ * @version : Appl.java, v 0.1 2022年07月28日 14:52 wjw01019072 Exp $
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ShopServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopServiceApplication.class, args);
+    }
+
+}
