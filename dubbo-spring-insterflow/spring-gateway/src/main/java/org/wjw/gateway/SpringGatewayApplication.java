70866321d05486f16b1ead40c5b40622cd8a37f2
隔壁老王 <1103450854@qq.com>
2022-12-03 11:22:53 +0800

    spring 方面初始化

--- dubbo-spring-insterflow/spring-gateway/src/main/java/org/wjw/gateway/SpringGatewayApplication.java
+++ dubbo-spring-insterflow/spring-gateway/src/main/java/org/wjw/gateway/SpringGatewayApplication.java
@@ -3,12 +3,14 @@ package org.wjw.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author laoWang
  * @ClassName SpringServiceApplication.java
  * @createTime 2022-12-02 19:45
  */
+@EnableFeignClients(basePackages = "org.wjw.api.**")
 @EnableDiscoveryClient
 @SpringBootApplication
 public class SpringGatewayApplication {
